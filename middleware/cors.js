57aa7b4f275f2fe5ab3ddc248d5365b0e761e8a3
kiniorap <li.carlosduarte98@gmail.com>
2025-05-17 11:14:17 -0700

    log del cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,9 @@
 export const corsMiddleware = (req, res) => {
   let  allowedOrigin = "";
+  console.log(req.headers.origin)
   if (req.headers.origin === process.env.FRONTEND_ORIGINS) {
     allowedOrigin = process.env.FRONTEND_ORIGINS;
-  }/* else if (req.headers.origin === "http://localhost:5173") {
-    allowedOrigin = process.env.FRONTEND_ORIGINS;
-  }*/
+  }
 
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
